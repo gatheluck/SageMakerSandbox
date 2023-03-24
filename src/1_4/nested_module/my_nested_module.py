000c4a8e07b699128a57342eda62f8ef4303175e
gatheluck <gatheluck+tech@gmail.com>
2023-03-25 03:01:55 +0900

    style: apply formatter.

--- src/1_4/nested_module/my_nested_module.py
+++ src/1_4/nested_module/my_nested_module.py
@@ -1,4 +1,4 @@
-def version_check(module) -> None:
+def version_check(module) -> None:  # type: ignore
     print("my_nested_module.version_check is called.")
     print(f" {module.__name__} version is {module.__version__}")
     return None
