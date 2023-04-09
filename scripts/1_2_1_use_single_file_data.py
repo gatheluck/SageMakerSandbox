db2f2cb612059709a24aa3fa9d4c0f0a0a466f95
gatheluck <gatheluck+tech@gmail.com>
2023-04-09 20:52:15 +0900

    feat: add local mode.

--- scripts/1_2_1_use_single_file_data.py
+++ scripts/1_2_1_use_single_file_data.py
@@ -1,6 +1,7 @@
 import os
 
 import sagemaker
+from sagemaker.local import LocalSession
 from sagemaker.pytorch import PyTorch
 
 if __name__ == "__main__":
@@ -17,20 +18,32 @@ if __name__ == "__main__":
                 f"Please specify value of `{_key}` as environmental variable."
             )
 
-    # uplaod data to S3 bucket
-    # https://sagemaker.readthedocs.io/en/stable/api/utility/session.html#sagemaker.session.Session.upload_data
-    input_s3_uri = sagemaker.session.Session().upload_data(
-        path="./data/1_2_1/calc.txt",
-        bucket=sagemaker.session.Session().default_bucket(),
-        key_prefix="training/1_2_1",
-    )
-    print(input_s3_uri)
+    # Specify if use local mode or not.
+    # https://sagemaker.readthedocs.io/en/stable/overview.html?highlight=local%20mode#local-mode
+    use_local = True
+    print(f"use_local: {use_local}")
 
-    use_local = False
+    if use_local:
+        sagemaker_session = LocalSession()
+        sagemaker_session.config = {'local': {'local_code': True}}
+
+        input_uri = "file://./data/1_2_1/calc.txt"
+    else:
+        # uplaod data to S3 bucket
+        # https://sagemaker.readthedocs.io/en/stable/api/utility/session.html#sagemaker.session.Session.upload_data
+        input_uri = sagemaker.session.Session().upload_data(
+            path="./data/1_2_1/calc.txt",
+            bucket=sagemaker.session.Session().default_bucket(),
+            key_prefix="training/1_2_1",
+        )
+
+    print(f"input_uri: {input_uri}")
+
 
     # https://sagemaker.readthedocs.io/en/stable/frameworks/pytorch/sagemaker.pytorch.html#sagemaker.pytorch.estimator.PyTorch
     estimator = PyTorch(
-        entry_point="./src/1_2_1/calc.py",
+        entry_point="calc.py",
+        source_dir="./src/1_2_1",
         py_version="py38",
         # https://github.com/aws/deep-learning-containers/blob/master/available_images.md#prior-sagemaker-framework-container-versions
         framework_version="1.12.1",
@@ -39,5 +52,5 @@ if __name__ == "__main__":
         instance_type="local" if use_local else "ml.m5.xlarge",  # 0.298 USD/h
         role=_role,
     )
-    estimator.fit(input_s3_uri)
+    estimator.fit(input_uri)
     print(estimator.latest_training_job.describe())
